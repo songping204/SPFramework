84869ea98f7cd0e48abdaf4ffdbf413403ae6cd9
songping <1664902286@qq.com>
2025-07-16 11:07:05 +0800

    test

---
+++ SPFrameworkDemo.xcframework/ios-arm64/SPFrameworkDemo.framework/Headers/Cat.h
@@ -0,0 +1,18 @@
+//
+//  Cat.h
+//  SPFrameworkDemo
+//
+//  Created by ping song on 2025/7/15.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Cat : NSObject
+
+-(void)doSomething;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SPFrameworkDemo.xcframework/ios-arm64_x86_64-simulator/SPFrameworkDemo.framework/Headers/SPFrameworkDemo.h
@@ -0,0 +1,18 @@
+//
+//  SPFrameworkDemo.h
+//  SPFrameworkDemo
+//
+//  Created by ping song on 2025/7/15.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SPFrameworkDemo.
+FOUNDATION_EXPORT double SPFrameworkDemoVersionNumber;
+
+//! Project version string for SPFrameworkDemo.
+FOUNDATION_EXPORT const unsigned char SPFrameworkDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SPFrameworkDemo/PublicHeader.h>
+
+#import <SPFrameworkDemo/Cat.h>
